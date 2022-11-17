d3d4be2dd9546251f4afd60cff7e9976d92cdf4a
shanthikaruna <shanthi.karuna01@gmail.com>
2022-11-17 13:21:14 -0800

    mobile responsiveness added for table component

--- src/components/calendar/DietCalendar.js
+++ src/components/calendar/DietCalendar.js
@@ -1,15 +1,23 @@
 import DaysList from "./DaysList";
+import "../../App.css";
 import "./DietCalendar.css";
 
 export default function DietCalendar() {
   return (
     <div className="DietCalendar-container">
       <table>
-        <tr className="DietCalendar-banner">
+        <tr>
+          <th></th>
+          <th>Breakfast</th>
+          <th>Lunch</th>
+          <th>Dinner</th>
+        </tr>
+        {/*<tr className="DietCalendar-banner">
+          <td className="DietCalendar-banner-item">Day</td>
           <td className="DietCalendar-banner-item">Breakfast</td>
           <td className="DietCalendar-banner-item">Lunch</td>
           <td className="DietCalendar-banner-item">Dinner</td>
-        </tr>
+          </tr>*/}
         <DaysList />
       </table>
     </div>
