932fba91fe5e9eb56c2402c4da5962b18f344a7c
Remilekun-Odegbami <remilekunodegbami@gmail.com>
2022-11-17 12:43:35 +0100

    added watch page

---
+++ src/components/FileUpload.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function FileUpload() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="form-group">
+                    <button className='btn upload'>Upload Video</button>
+
+                </div>
+                <div className="typeContent">
+                    <div className="watch">
+                        <div className="mb-3">
+                            <label htmlFor="fileInput" className='cloud'>
+                                <i className="fa fa-cloud"></i>
+                            </label>
+                            <h5 className='text text-center mt-3'>Click on icon to upload videos</h5>
+                            <input type="file" className="d-none" id="fileInput" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/types/vegetarians/Vegetarians.jsx
+++ src/components/types/vegetarians/Vegetarians.jsx
@@ -3,10 +3,12 @@ import Breakfast from './Breakfast'
 import Dinner from './Dinner'
 import Lunch from './Lunch'
 import { Link } from 'react-router-dom';
+import Welcome from '../../Welcome';
 
 export default function Vegetarians() {
     return (
-        <div className='container mt-5'>
+        <div className='container'>
+            <Welcome />
             <div className="row typeContent">
                 <h2 className="heading">Vegertarians</h2>
                 <p className="text">A vegetarian (a.k.a. herbivore) is a person who does not eat any type of meat and eats a plant-based diet instead.</p>
@@ -14,8 +16,8 @@ export default function Vegetarians() {
                 <Lunch />
                 <Dinner />
                 <div className="btns">
-                    <Link to className="btn">Diet Calender</Link>
-                    <a href='' className="btn">Watch</a>
+                    <Link to='' className="btn">Diet Calender</Link>
+                    <Link to="/watch" className="btn">Watch</Link>
                 </div>
             </div>
         </div>
