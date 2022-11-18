cc2dc58d0dd0274c4268fbf6cd71109fd17e7c33
Shalom Ijeoma Mathew <codabae@Shaloms-MacBook-Pro-2.local>
2022-11-18 16:03:50 +0100

    made an update

--- src/components/types/pescetarians/Lunch.jsx
+++ src/components/types/pescetarians/Lunch.jsx
@@ -6,11 +6,10 @@ export default function Lunch() {
             <div className="row" >
                 <h2 className="subheading">Lunch</h2>
                 <ul className="list">
-                    <li className="text">Cereal and toast</li>
-                    <li className="text">Cereal and toast</li>
-                    <li className="text">Cereal and toast</li>
-                    <li className="text">Cereal and toast</li>
-                    <li className="text">Cereal and toast</li>
+                    <li className="text">Mackerel and Green Bean Salad,</li>
+                    <li className="text">Baked Tilapia Fish Tacos,</li>
+                    <li className="text">Smoked Salmon Salad with Farro & Goat Cheese,</li>
+                    <li className="text">Hawaiian Ahi Poke Bowl</li>
                 </ul>
             </div>
         </div>
