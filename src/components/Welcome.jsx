61ec64723b86757e92b263bcf618844f0af918f5
Shalom Ijeoma Mathew <codabae@Shaloms-MacBook-Pro-2.local>
2022-11-18 16:37:38 +0100

    made an update

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,7 +5,7 @@ import Logo from '../assets/EatGramHorizontalLogo.png'
 export default function Welcome() {
     return (
         <div className='container'>
-            <img src={logo} alt='logo'/>
+            <img src={Logo} alt='logo'/>
             <h1 className="main-heading heading">Hello and welcome to <span style={{ color: '#249644' }}>EatGram🥘</span></h1>
             <p className="about">
                 EatGram is a social media platform where different types of eaters can learn, network, follow a diet plan, and watch food-related videos.</p>
